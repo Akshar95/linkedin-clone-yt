9544a80b05133231a697a434f53446455a6b1a9e
Akshar Patel <patel.akshar295@gmail.com>
2021-07-21 06:13:29 +0100

    Header Left

---
+++ src/HeaderOption.js
@@ -0,0 +1,13 @@
+import './HeaderOption.css'
+
+function HeaderOption({Icon, title}) {
+    return (
+        <div className='HeaderOption'>
+            {Icon && <Icon className='HeaderOption__icon' />}
+            <h3 className='HeaderOption__title'>{title}</h3>
+            
+        </div>
+    )
+}
+
+export default HeaderOption
