c9dc9fa96e5374c29e0e8a40b8ba934f186c567b
Akshar Patel <patel.akshar295@gmail.com>
2021-07-21 21:44:21 +0100

    Initial Sidebar top/stats/bottom build

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,31 @@
 import './Sidebar.css';
+import { Avatar } from '@material-ui/core'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>This is the sidebar</h1>
+            <div className="sidebar__top">
+                <img src="" alt="" />
+                <Avatar className='sidebar__avatar'/>
+                <h2>Akshar Patel</h2>
+                <h4>This is my LinkedIn Page</h4>
+            </div>
+
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who Viewed You</p>
+                    <p className="sidebar_statNumber">8,294</p>
+                </div>
+
+                <div className="sidebar__stat">
+                    <p>Views On Post</p>
+                    <p className="sidebar_statNumber">2,334</p>
+                </div>
+            </div>
+
+            <div className="sidebar__bottom">
+                <p>Recent</p>
+            </div>
         </div>
     )
 }
