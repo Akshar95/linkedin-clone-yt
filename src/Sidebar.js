40be78b70ead27bc8b470b162069b7560b71e91b
Akshar Patel <patel.akshar295@gmail.com>
2021-07-24 19:26:17 +0100

    styling sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,7 +5,8 @@ function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebar__top">
-                <img src="" alt="" />
+            <img src=
+            'https://images.unsplash.com/photo-1583263201272-f4e5bedc2f36?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80' alt="scene" />
                 <Avatar className='sidebar__avatar'/>
                 <h2>Akshar Patel</h2>
                 <h4>This is my LinkedIn Page</h4>
